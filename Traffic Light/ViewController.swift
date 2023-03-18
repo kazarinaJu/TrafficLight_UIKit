cdb85598c10cf579876b7d2ec2d406f9a49b978b
Julia Kazarina <yastrebovayv@gmail.com>
2023-03-18 07:58:29 +0500

    switch traffic light colors

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -12,19 +12,45 @@ class ViewController: UIViewController {
     @IBOutlet var lightViews: [UIView]!
     @IBOutlet var startButton: UIButton!
     
+    private enum TrafficLightColors {
+            case red, yellow, green
+        }
+        
+    private var currentColor:TrafficLightColors = .red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         startButton.layer.cornerRadius = 10
         
         for lightView in lightViews {
-            lightView.layer.cornerRadius = lightView.frame.size.width/2
+            lightView.layer.cornerRadius = lightView.frame.width/2
         }
     }
 
     @IBAction func startButtonDidTapped() {
-        startButton.setTitle("NEXT", for: .normal)
-    }
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+            lightViews[0].alpha = 1
+            return
+        }
+        
+        switch currentColor {
+        case .red:
+            lightViews[0].alpha = 0.3
+            lightViews[1].alpha = 1
+            currentColor = .yellow
+        case .yellow:
+            lightViews[1].alpha = 0.3
+            lightViews[2].alpha = 1
+            currentColor = .green
+        case .green:
+            lightViews[2].alpha = 0.3
+            lightViews[0].alpha = 1
+            currentColor = .red
+        }
+        
+        }
     
 }
 
